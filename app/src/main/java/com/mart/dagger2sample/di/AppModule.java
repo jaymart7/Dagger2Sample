952e8ef3c43be5ea781bbcc306547342bb5f770a
Jaymart <jaymart@thethumbfactory.com.au>
2018-07-20 13:28:16 +0800

    ApiClient moved to AppModule

--- app/src/main/java/com/mart/dagger2sample/di/AppModule.java
+++ app/src/main/java/com/mart/dagger2sample/di/AppModule.java
@@ -4,19 +4,38 @@ import android.app.Application;
 import android.content.Context;
 
 import com.mart.dagger2sample.data.UserRepository;
-import com.mart.dagger2sample.data.network.ApiClient;
 import com.mart.dagger2sample.data.network.ApiService;
+
+import java.util.concurrent.TimeUnit;
 
 import dagger.Binds;
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 public abstract class AppModule {
 
     @Provides
     static ApiService getApiService() {
-        return ApiClient.getClient();
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient
+                .connectTimeout(5, TimeUnit.MINUTES)
+                .writeTimeout(5, TimeUnit.MINUTES)
+                .readTimeout(5, TimeUnit.MINUTES)
+                .addInterceptor(logging);
+
+        return new Retrofit.Builder()
+                .baseUrl("https://randomuser.me/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build())
+                .build().create(ApiService.class);
     }
 
     @Provides
--- app/src/main/java/com/mart/dagger2sample/model/User.java
+++ app/src/main/java/com/mart/dagger2sample/model/User.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName;
 public class User {
 
     @SerializedName("email")
-    String email;
+    private String email;
 
     public String getEmail() {
         return email;
